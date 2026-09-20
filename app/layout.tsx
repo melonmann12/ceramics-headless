ff22fc1c3f20996427cf5df1007f790b013544ba
melonmann12 <hongquanvjp@gmail.com>
2026-09-20 23:38:03 +0700

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
         </CartProvider>
         <ShopifyAnalytics />
         {process.env.META_PIXEL_ID && <MetaPixel pixelId={process.env.META_PIXEL_ID} />}
-        <GorgiasChat />
+        {/* <GorgiasChat /> - Temporarily disabled chatbox, can be re-enabled later */}
       </body>
     </html>
   );
